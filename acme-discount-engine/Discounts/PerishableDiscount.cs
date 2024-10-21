9c0966bdf6d169d3dc7f2c0cd8e8a5657aaf8e41
Femi Ashiru <femi.ashiru@bjss.com>
2024-10-21 13:34:52 +0100

    create perishable discount class

---
+++ acme-discount-engine/Discounts/PerishableDiscount.cs
@@ -0,0 +1,72 @@
+﻿using AcmeSharedModels;
+
+namespace acme_discount_engine.Discounts
+{
+    public class PerishableDiscount : IDiscount
+    {
+        public DateTime Time { get; set; } = DateTime.Now;
+
+        public void CalculateDiscount(List<Item> itemList, ItemDiscountDictionary itemDiscounts)
+        {
+            itemList.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            foreach (Item item in itemList)
+            {
+                int daysUntilDate = SetDaysUntil(item);
+
+                if (item.IsPerishable && daysUntilDate == 0)
+                {
+                    if (Time.Hour > 17)
+                    {
+                        item.Price -= item.Price * (!item.Name.Contains("(Meat)") ? 0.25 : 0.15);
+                    }
+                    else if (Time.Hour > 15)
+                    {
+                        item.Price -= item.Price * 0.15;
+                    }
+                    else if (Time.Hour > 11)
+                    {
+                        item.Price -= item.Price * 0.10;
+                    }
+                    else
+                    {
+                        item.Price -= item.Price * 0.05;
+                    }
+                }
+
+                if (!item.IsPerishable && !itemDiscounts.discounts["NoDiscount"].Contains(item.Name))
+                {
+
+                    if (daysUntilDate < 0)
+                    {
+                        item.Price -= item.Price * 0.20;
+                    }
+
+                    else if (daysUntilDate < 6)
+                    {
+                        item.Price -= item.Price * 0.10;
+                    }
+
+                    else if (daysUntilDate < 11)
+                    {
+                        item.Price -= item.Price * 0.05;
+                    }
+
+                }
+
+            }
+        }
+        public int SetDaysUntil(Item item)
+        {
+            int daysUntilDate = (item.Date - DateTime.Today).Days;
+            if (DateTime.Today > item.Date) { daysUntilDate = -1; }
+            return daysUntilDate;
+        }
+        public void CalculateDiscount(List<Item> itemList)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
