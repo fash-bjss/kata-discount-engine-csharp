7b458fe534631935d8a83b0f3800e03f207d69ba
Femi Ashiru <femi.ashiru@bjss.com>
2024-10-24 09:26:46 +0100

    add Money class

---
+++ acme-discount-engine/Discounts/Money.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acme_discount_engine.Discounts
+{
+    public class Money
+    {
+        private double _value {  get; }
+        public Money(double value) {
+            _value = value;
+        }
+    }
+}
